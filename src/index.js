883c72a3f5e87e4e0ff8ba9f7c0fbecbbfb65230
Nadya1311 <nadya.samosyuk.88@mail.ru>
2022-01-18 14:27:24 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let m = [];
+    if(n<0){
+        let a = String(n*(-1))
+        for(let i=1; i<a.length+1; i++){
+            m.push(a[a.length-i])
+        }
+    }else{
+        let a = String(n);
+        for(let i=1; i<a.length+1; i++){
+            m.push(a[a.length-i])
+        }
+    }
+    return m.join('')
 }
